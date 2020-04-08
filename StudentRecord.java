e0b5338c8368bb277494e55d8dc0dadd94e3e614
Hridyanshu <hridyanshusharma@gmail.com>
2020-04-08 23:59:17 +0530

    Aggregation code to store the details of the students and display the same.

--- StudentRecord.java
+++ StudentRecord.java
@@ -37,4 +37,38 @@ class Date
 		System.out.println("Date of Birth: " + this.dd + "-" + this.mm + "-" + this.yyyy);
 	}
 }
-
+public class StudentRecord
+{
+	public static void main(String args[])
+	{
+		String name;
+		int sapId, dd, mm, yyyy;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the number of students: ");
+		int size = input.nextInt(); //size refers to the no of students.
+		Student student[] = new Student[size]; //array of refernces.
+		for(int i=0;i<size;i++)
+		{
+			//Taking data of students from the user.
+			System.out.println("Enter the details of the student " + (i+1));
+			System.out.print("Name: ");
+			input.nextLine();
+			name = input.nextLine();
+			System.out.print("Sap-Id: ");
+			sapId = input.nextInt();
+			System.out.print("Date of Birth (dd mm yyyy): ");
+			dd = input.nextInt();
+			mm = input.nextInt();
+			yyyy = input.nextInt();
+			student[i] = new Student(name, sapId, dd, mm, yyyy);
+		}
+		System.out.println("\n\nDisplaying the details of the students");
+		for(int i=0;i<size;i++)
+		{
+			//Displaying the data of students to the user.
+			System.out.println("Student " + (i+1));
+			student[i].display();
+			System.out.print("\n");
+		}
+	}
+}
